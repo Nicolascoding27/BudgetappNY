4ce7fa04b95f386ef74072a1a5cfa225689a391c
Nicolascoding27 <nicolas.diaz27@hotmail.com>
2021-02-28 17:29:03 -0500

    Expense method done

--- js/app.js
+++ js/app.js
@@ -80,6 +80,30 @@ class UI {
       //we have to show balance
     }
   }
+  //add expense method
+  addExpense(expense){
+      const div= documment.createElement('div');
+      div.classList.add('expense'); //add the class, this class is in index.html
+      div.innerHTML=`
+      <div class="expense-item d-flex justify-content-between align-items-baseline">
+
+         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+
+         <div class="expense-icons list-item">
+
+          <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+           <i class="fas fa-edit"></i>
+          </a>
+          <a href="#" class="delete-icon" data-id="${expense.id}">
+           <i class="fas fa-trash"></i>
+          </a>
+         </div>
+        </div>
+        `;
+        this.expenseList.appendChild(div);
+        
+  }
   totalExpense(){
     let total = 400;
     return total;
